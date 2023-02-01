92e83f7481cef5b5ef239525f344c260a79bef92
viveksangwan-cmd <viveksangwan5791@gmail.com>
2023-02-01 15:31:17 +0530

    Initialized project
    Added Spring Data JDBC, Spring JPA,H2 Database and Spring Web

---
+++ databasedemo/src/test/java/com/viveksangwan/database/databasedemo/DatabasedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viveksangwan.database.databasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
